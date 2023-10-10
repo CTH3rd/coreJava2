85b2093c33c63a2cbe44fd8552777b38dca4984c
lesss <lesss@CTHSolutions_23.lan>
2023-10-10 16:53:21 -0400

    Collection Class added

---
+++ src/collections/Collections.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package collections;
+
+/**
+ * 
+ */
+public class Collections {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
